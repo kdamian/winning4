24a5220c01d3d5357630dc97acc9b4b825d3aed1
kdamian <blinkinruzdecaber@gmail.com>
2016-12-11 22:34:16 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+// movements counter
+function counter() {
+    for (var i = 1; i <= 42; i++) {
+        if (i % 2 = 0) {
+            redField();
+            i++;
+        } else {
+            blueField();
+            i++;
+        }
+    };
+};
+
+
+//##############################################################
+
+// function adds class with red circle
+function redField() {
+  $(document).ready(function() {
+    $(this).click(function() {
+      field_id = $(this).attr('id');
+      $(field_id).toggleClass('red');
+    });
+  });
+};
+
+// function adds class with blue circle
+function blueField() {
+  $(document).ready(function() {
+    $(this).click(function() {
+      field_id = $(this).attr('id');
+      $('#' + field_id).toggleClass('blue');
+    });
+  });
+};
+
+blueField();
+redField();
+
+
+// $(document).ready(function() {
+//   $(this).click(function() {
+//     $('#1').toggleClass('blue');
+//   });
+// });
